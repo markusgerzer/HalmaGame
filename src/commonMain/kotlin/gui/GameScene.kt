5b3c81fdd08b4b6a7ace90fbbe541bc91ccdd0f3
Markus Gerzer <mg@markusgerzer.de>
2022-11-10 12:05:54 +0100

    enabled zoom

--- src/commonMain/kotlin/gui/GameScene.kt
+++ src/commonMain/kotlin/gui/GameScene.kt
@@ -4,18 +4,19 @@ import com.soywiz.korge.scene.*
 import com.soywiz.korge.view.*
 import com.soywiz.korio.async.*
 import halma.*
+import zoom.*
 
 class GameScene : Scene() {
     override suspend fun SContainer.sceneInit() {
     }
 
     override suspend fun SContainer.sceneMain() {
-        val gameParameter = injector.get<GameParameter<StarhalmaBoardGui, StarhalmaBoard>>()
+        addZoomComponent(ZoomComponent(this))
 
-        val game = makeGame(gameParameter)
-
+        val game = makeGame(injector.get<GameParameter<StarhalmaBoardGui, StarhalmaBoard>>())
         game.board.onExit { launchImmediately { sceneContainer.back() } }
         game.start()
+
         sceneContainer.back()
     }
 }
