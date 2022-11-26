259ba8f86b3a639055f97b6fc09f899c3b221995
Markus Gerzer <mg@markusgerzer.de>
2022-11-26 19:18:13 +0100

    use one onclick listener for all fields

--- src/commonMain/kotlin/gui/StarhalmaBoardBackground.kt
+++ src/commonMain/kotlin/gui/StarhalmaBoardBackground.kt
@@ -1,23 +1,41 @@
 package gui
 
-import com.soywiz.korge.view.Container
-import com.soywiz.korge.view.anchor
-import com.soywiz.korge.view.graphics
-import com.soywiz.korge.view.xy
-import com.soywiz.korim.color.Colors
+import com.soywiz.korge.input.*
+import com.soywiz.korge.view.*
+import com.soywiz.korim.color.*
+import com.soywiz.korio.async.*
 import com.soywiz.korma.geom.Point
-import com.soywiz.korma.geom.vector.StrokeInfo
-import com.soywiz.korma.geom.vector.line
-import com.soywiz.korma.geom.vector.polygon
-import halma.StarhalmaStaticBoardMappings
+import com.soywiz.korma.geom.vector.*
+import halma.*
 
-fun Container.starhalmaBoardBackground() = graphics( {
+fun Graphics.starhalmaMarks() = List(StarhalmaStaticBoardMappings.fieldsSize) {
+    circle {
+        radius = StarhalmaBoardGuiConfig.MARK_RADIUS
+        stroke = Colors.GREEN
+        strokeThickness = StarhalmaBoardGuiConfig.MARK_LINE_THICKNESS
+        alpha = 0.5
+        visible = false
+        anchor(.5, .5)
+        val offset = Point(strokeThickness, strokeThickness) / 2
+        xy(StarhalmaBoardGuiConfig.fieldCoordinates0[it] - StarhalmaBoardGuiConfig.midpoint + offset)
+    }
+}
+
+
+fun Container.starhalmaBoardBackground(
+    onEmptyFieldClicked: AsyncSignal<Int>
+) = graphics( {
+    val offset = Point(
+        StarhalmaBoardGuiConfig.xFactor - StarhalmaBoardGuiConfig.FIELD_RADIUS,
+        StarhalmaBoardGuiConfig.yFactor - StarhalmaBoardGuiConfig.FIELD_RADIUS
+    )
+
     fun polygonOfCoordinateIndices(vararg indices: Int) {
         polygon(indices.map { i ->
-            StarhalmaBoardGuiConfig.fieldCoordinates0[i] -
-                    Point(StarhalmaBoardGuiConfig.xFactor, StarhalmaBoardGuiConfig.yFactor)
+            StarhalmaBoardGuiConfig.fieldCoordinates0[i] - offset
         })
     }
+
     // Red Homes
     fill(Colors.RED) {
         polygonOfCoordinateIndices(0, 18, 14)
@@ -38,16 +56,37 @@ fun Container.starhalmaBoardBackground() = graphics( {
         for (i in 0 until StarhalmaStaticBoardMappings.fieldsSize) {
             for (n in StarhalmaStaticBoardMappings.fieldNeighbors[i]) {
                 if (n > i) {
-                    val a = StarhalmaBoardGuiConfig.fieldCoordinates0[i] -
-                            Point(StarhalmaBoardGuiConfig.xFactor, StarhalmaBoardGuiConfig.yFactor)
-                    val b = StarhalmaBoardGuiConfig.fieldCoordinates0[n] -
-                            Point(StarhalmaBoardGuiConfig.xFactor, StarhalmaBoardGuiConfig.yFactor)
+                    val a = StarhalmaBoardGuiConfig.fieldCoordinates0[i] - offset
+                    val b = StarhalmaBoardGuiConfig.fieldCoordinates0[n] - offset
                     line(a, b)
                 }
             }
         }
     }
+    // Fields
+    fill(Colors.BLACK) {
+        for (i in 0 until StarhalmaStaticBoardMappings.fieldsSize) {
+            circle(
+                StarhalmaBoardGuiConfig.fieldCoordinates0[i] - offset,
+                StarhalmaBoardGuiConfig.FIELD_RADIUS
+            )
+        }
+    }
+    fill(Colors.YELLOW) {
+        for (i in StarhalmaStaticBoardMappings.middleFields)
+            circle(
+                StarhalmaBoardGuiConfig.fieldCoordinates0[i] - offset,
+                StarhalmaBoardGuiConfig.FIELD_RADIUS - 2 * StarhalmaBoardGuiConfig.LINE_THICKNESS
+            )
+    }
 } ) {
     xy(StarhalmaBoardGuiConfig.midpoint)
     anchor(0.5, 0.5)
+    onClick {
+        for ((i, p) in StarhalmaBoardGuiConfig.fieldCoordinates0Scaled.withIndex()) {
+            if(p.distanceTo(it.currentPosStage) <= StarhalmaBoardGuiConfig.FIELD_RADIUS) {
+                onEmptyFieldClicked(i)
+            }
+        }
+    }
 }
